ed058d2b17085bfa4e532bb8825f9076ea1b172b
francesco.manghi <francesco.manghi@outlook.it>
2018-07-12 15:38:18 +0200

    a

--- Photo.py
+++ Photo.py
@@ -10,66 +10,63 @@ import ftplib
 
 
 with open('Config_device.json', 'r') as f:
-    config = json.load(f)
-    for elem in config:
-        locals()['{0}'.format(elem)]=config[elem]
+	config = json.load(f)
+	for elem in config:
+		locals()['{0}'.format(elem)]=config[elem]
 
 CODE= COMMITTENTE + '_' + CPUID
 
 
 def PiCameraFilter(camera):
-    print('Setting camera filter')
-    camera.brightness = CAM_brightness #0>freddo 100>caldo
-    camera.sharpness = CAM_sharpness #0>freddo 100>caldo
-    camera.contrast = CAM_contrast
-    camera.saturation = CAM_saturation
-    camera.ISO = CAM_ISO
-    camera.video_stabilization = CAM_video_stabilization
-    camera.exposure_compensation = CAM_exposure_compensation
-    camera.exposure_mode = CAM_exposure_mode
-    camera.meter_mode = CAM_meter_mode
-    camera.awb_mode = CAM_awb_mode
-    camera.image_effect = CAM_image_effect
-    camera.color_effects = CAM_color_effects
-    camera.rotation = CAM_rotation
-    camera.hflip = CAM_hflip
-    camera.vflip = CAM_vflip
-    camera.resolution = (CAM_resolution[0], CAM_resolution[1])
-    return camera
+	print('Setting camera filter')
+	camera.brightness = CAM_brightness #0>freddo 100>caldo
+	camera.sharpness = CAM_sharpness #0>freddo 100>caldo
+	camera.contrast = CAM_contrast
+	camera.saturation = CAM_saturation
+	camera.ISO = CAM_ISO
+	camera.video_stabilization = CAM_video_stabilization
+	camera.exposure_compensation = CAM_exposure_compensation
+	camera.exposure_mode = CAM_exposure_mode
+	camera.meter_mode = CAM_meter_mode
+	camera.awb_mode = CAM_awb_mode
+	camera.image_effect = CAM_image_effect
+	camera.color_effects = CAM_color_effects
+	camera.rotation = CAM_rotation
+	camera.hflip = CAM_hflip
+	camera.vflip = CAM_vflip
+	camera.resolution = (CAM_resolution[0], CAM_resolution[1])
+	return camera
 
 def TakePhoto(CODE,directory):
-    camera = PiCamera()
-    camera = PiCameraFilter(camera)
-    filename = datetime.now().strftime("%Y%m%d%H%M_" + CODE + ".jpg")
-    print('Taking foto '+ str(filename))
-    camera.capture(directory+ '/'+filename)
-    camera.close()
+	camera = PiCamera()
+	camera = PiCameraFilter(camera)
+	filename = datetime.now().strftime("%Y%m%d%H%M_" + CODE + ".jpg")
+	print('Taking foto '+ str(filename))
+	camera.capture(directory+ '/'+filename)
+	camera.close()
 
 
 def Clean(SendDirectory):
-    for elem in os.listdir(SendDirectory):
-        if (str(elem.split('_')[0])[:8]) <> (datetime.now().strftime("%Y%m%d")):
-            os.remove(SendDirectory+'/'+elem)
+	for elem in os.listdir(SendDirectory):
+		if (str(elem.split('_')[0])[:8]) <> (datetime.now().strftime("%Y%m%d")):
+			os.remove(SendDirectory+'/'+elem)
 
 
 def SendFTP(directory,SendDirectory):
    for filename in os.listdir(directory):
-       print('Sending image:\n'+directory)
-       session = ftplib.FTP('104.40.239.18','waterberry','waterberry_ftp_vm')
-       immagine = open(directory+'/'+filename,'rb')
-       print(directory+'/'+filename)
-       session.storbinary('STOR '+ filename, immagine)
-       print('STOR '+ filename)
-       immagine.close()
-       session.quit()
-       print('Immagine inviata...')
-       os.rename(directory + '/' + filename,SendDirectory + '/' + filename)
-       print('Image moved to sent folder')
+	   print('Sending image:\n'+directory)
+	   session = ftplib.FTP('104.40.239.18','waterberry','waterberry_ftp_vm')
+	   immagine = open(directory+'/'+filename,'rb')
+	   print(directory+'/'+filename)
+	   session.storbinary('STOR '+ filename, immagine)
+	   print('STOR '+ filename)
+	   immagine.close()
+	   session.quit()
+	   print('Immagine inviata...')
+	   os.rename(directory + '/' + filename,SendDirectory + '/' + filename)
+	   print('Image moved to sent folder')
 
-	   
-os.makedirs('/data/Nuove_Foto', exist_ok=True)
-os.makedirs('/data/Foto_Inviate', exist_ok=True)
-
+	
 print('Starting')
 directory= '/data/Nuove_Foto'
 SendDirectory = '/data/Foto_Inviate'
